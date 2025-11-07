c5250139b18e279946ebbbe3212c576763dc6644
Huy Vu2 <huvu@login-eos02.eos.clusters.nvidia.com>
2025-11-07 15:48:52 -0800

    update dfm/src/megatron/data/dit

--- dfm/src/megatron/data/dit/diffusion_energon_datamodule.py
+++ dfm/src/megatron/data/dit/diffusion_energon_datamodule.py
@@ -14,6 +14,7 @@
 
 # pylint: disable=C0115,C0116,C0301
 
+
 import logging
 from dataclasses import dataclass
 from typing import Any, Dict, Literal
@@ -22,8 +23,8 @@ from megatron.bridge.data.utils import DatasetBuildContext, DatasetProvider
 from megatron.energon import DefaultTaskEncoder, get_train_dataset
 from torch import int_repr
 
-from dfm.src.megatron.data.dit.base import EnergonMultiModalDataModule
-from dfm.src.megatron.data.dit.diffusion_taskencoder import BasicDiffusionTaskEncoder
+from dfm.src.megatron.data.dit.base_energon_datamodule import EnergonMultiModalDataModule
+from dfm.src.megatron.data.dit.dit_taskencoder import DiTTaskEncoder
 
 
 @dataclass(kw_only=True)
@@ -32,6 +33,7 @@ class DiffusionDataModuleConfig(DatasetProvider):
     seq_length: int
     micro_batch_size: int
     task_encoder_seq_length: int
+    packing_buffer_size: int
     global_batch_size: int
     num_workers: int_repr
     dataloader_type: str = "external"
@@ -40,14 +42,18 @@ class DiffusionDataModuleConfig(DatasetProvider):
         self.dataset = DiffusionDataModule(
             path=self.path,
             seq_length=self.seq_length,
-            task_encoder=BasicDiffusionTaskEncoder(seq_length=self.task_encoder_seq_length),
+            task_encoder=DiTTaskEncoder(
+                seq_length=self.task_encoder_seq_length, packing_buffer_size=self.packing_buffer_size
+            ),
             micro_batch_size=self.micro_batch_size,
+            packing_buffer_size=self.packing_buffer_size,
             global_batch_size=self.global_batch_size,
             num_workers=self.num_workers,
         )
         self.sequence_length = self.dataset.seq_length
 
     def build_datasets(self, context: DatasetBuildContext):
+        # TODO: add validation and test datasets
         return self.dataset.train_dataloader(), self.dataset.train_dataloader(), self.dataset.train_dataloader()
 
 
@@ -84,6 +90,7 @@ class DiffusionDataModule(EnergonMultiModalDataModule):
         global_batch_size: int = 8,
         num_workers: int = 1,
         pin_memory: bool = True,
+        packing_buffer_size: int = None,
         task_encoder: DefaultTaskEncoder = None,
         use_train_split_for_val: bool = False,
     ) -> None:
@@ -108,6 +115,7 @@ class DiffusionDataModule(EnergonMultiModalDataModule):
             micro_batch_size=micro_batch_size,
             global_batch_size=global_batch_size,
             num_workers=num_workers,
+            packing_buffer_size=packing_buffer_size,
             pin_memory=pin_memory,
             task_encoder=task_encoder,
         )
@@ -134,6 +142,7 @@ class DiffusionDataModule(EnergonMultiModalDataModule):
         _dataset = get_train_dataset(
             self.path,
             batch_size=self.micro_batch_size,
+            packing_buffer_size=self.packing_buffer_size,
             task_encoder=self.task_encoder,
             worker_config=worker_config,
             max_samples_per_sequence=None,
