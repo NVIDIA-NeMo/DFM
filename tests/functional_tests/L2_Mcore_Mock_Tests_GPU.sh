cce9cde5f87e07eaac36e70e386ac0552b5954af
linnan wang <wangnan318@gmail.com>
2025-11-17 15:17:46 -0800

    update
    Signed-off-by: linnan wang <wangnan318@gmail.com>

--- tests/functional_tests/L2_Mcore_Mock_Tests_GPU.sh
+++ tests/functional_tests/L2_Mcore_Mock_Tests_GPU.sh
@@ -11,4 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-CUDA_VISIBLE_DEVICES="0,1" uv run --group megatron-bridge coverage run -a --data-file=/opt/DFM/.coverage --source=/opt/DFM/ -m pytest tests/functional_tests/test_mcore_wan_pretrain.py -m "not pleasefixme" --with_downloads -v
+CUDA_VISIBLE_DEVICES="0,1" uv run --group megatron-bridge coverage run -a --data-file=/opt/DFM/.coverage --source=/opt/DFM/ -m pytest tests/functional_tests/mcore/recipes/test_wan_pretrain.py -m "not pleasefixme" --with_downloads -v
