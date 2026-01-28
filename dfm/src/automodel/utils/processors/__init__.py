0c2dea8d7987f06cba1f4fb5365c5a1602582178
Pranav Prashant Thombre <pthombre@nvidia.com>
2026-01-27 17:05:22 -0800

    Code fixes
    Signed-off-by: Pranav Prashant Thombre <pthombre@nvidia.com>

--- dfm/src/automodel/utils/processors/__init__.py
+++ dfm/src/automodel/utils/processors/__init__.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .flux import FluxProcessor
 from .base import BaseModelProcessor
 from .registry import ProcessorRegistry
-from .flux import FluxProcessor
 
 __all__ = [
     "BaseModelProcessor",
